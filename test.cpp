5dc52c12e90b8c865eb559724dcfd050b0cb0483
sanjaysinghrajawat <sanjaysinghrajawat77@gmail.com>
2023-03-20 12:05:56 +0530

    clear test file

--- test.cpp
+++ test.cpp
@@ -2,20 +2,6 @@
 using namespace std;
 int main()
 {
-    char name[10] = "sanjay";
-    char * cptr = &name[0];
 
-    cout << name << endl;
-    cout << &name << endl;
-    cout << *(name+3) << endl;
-    cout << cptr << endl;
-    cout << &cptr << endl;
-    cout << *(cptr+3) << endl;
-    cout << cptr+2 << endl;
-    cout << *cptr << endl;
-    cout << cptr+8 << endl;
-    
-    cout << "One done" << endl;
-    cout << "Two done" << endl;
     return 0;
 }
